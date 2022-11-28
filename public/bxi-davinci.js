811ac1b2c226ecb46ad8c5b2feb3f2b771016913
Johana Estefo <johana.estefo@gmail.com>
2022-11-28 10:51:52 -0300

    prueba scss node server

---
+++ public/bxi-davinci.js
@@ -0,0 +1,205 @@
+
+import FlowContainerWrapper from '/js/flow-container-wrapper.js';
+import initFunctionRegistry from '/js/function-registry.js';
+import Logger from '/js/logger.js';
+import registerFunctions from '/register-functions.js';
+
+(async function () {
+  const logger = new Logger(window._env_.BXI_DEBUG_LOGGING === 'true');
+  initFunctionRegistry(logger);
+  registerFunctions(logger);
+
+  let activeWidget = null;
+  let isStaticWidget = false;
+  let modal = null;
+
+  await checkContinueToken();
+
+  const flowTriggers = document.querySelectorAll('[data-dv-flow]');
+  flowTriggers.forEach(flowTrigger => {
+    logger.log(`Configuring flow trigger ${flowTrigger.dataset.dvFlow} for element`, flowTrigger);
+    switch (flowTrigger.dataset.dvFlow) {
+      case "modal":
+        flowTrigger.addEventListener('click', event => handleDvModalCallback(event.target, 'modal-widgetbox'));
+        break;
+      case "static":
+        isStaticWidget = true;
+        if (!activeWidget) {
+          launchStaticWidget(flowTrigger);
+        }
+        break;
+      default:
+        logger.warn('Invalid flow trigger detected valid values for data-dv-flow are "modal" or "static"');
+    }
+  });
+
+  async function checkContinueToken() {
+    // Check for continueToken
+    const urlParams = new URLSearchParams(window.location.search);
+    const continueToken = urlParams.get('continueToken');
+    if (continueToken) {
+      window.history.replaceState({}, document.title, window.location.pathname);
+      await handleContinueToken(continueToken);
+    } else {
+      // If there's no continueToken on page load we don't care about this
+      sessionStorage.removeItem('bxi_runningPolicyId');
+    }
+  }
+  
+  async function handleContinueToken(continueToken) {
+    const policyId = sessionStorage.getItem('bxi_runningPolicyId');
+    if (policyId) {
+      const el = document.querySelector(`[data-policy-id="${policyId}"]`);
+
+      if (!el) {
+        logger.warn(`Element with data-policy-id="${policyId}" was not found, flow could not be resumed`);
+      }
+
+      const wrapper = new FlowContainerWrapper(el);
+
+      if (wrapper.DvFlowType === 'modal') {
+        showModal(wrapper);
+        await launchFlow(document.getElementById('modal-widgetbox'), wrapper, continueToken);
+      } else if (wrapper.DvFlowType === 'static') {
+        launchStaticWidget(el, continueToken);
+      } else {
+        logger.warn('Flow could not be continued, invalid data-dv-flow type encountered, valid values for data-dv-flow are "modal" or "static"');
+      }
+    } else {
+      logger.warn('Flow could not be continued because there was no bxi_runningPolicyId in session storage');
+    }
+  }
+
+  async function handleDvModalCallback(target, flowContainerId) {
+    const widgetWrapper = new FlowContainerWrapper(target);
+
+    if (!widgetWrapper.PolicyId) {
+      throw Error('Policy ID is required to launch a flow.');
+    }
+
+    let buttonEventName = target.alt === 'Remix on glitch' ? target.alt : target.innerText;
+    buttonEventName = buttonEventName.toLowerCase().split(' ').join('_') + '_clicked';
+    
+    window.bxi.sendAnalytics?.(buttonEventName);
+
+    await launchFlow(document.getElementById(flowContainerId), widgetWrapper);
+
+    showModal(widgetWrapper);
+  }
+
+  function showModal(widgetWrapper) {
+    const modalEl = document.getElementById('dv-modal');
+    if (widgetWrapper.HideLogo) {
+      modalEl.classList.add('hide-vertical-logo');
+    }
+
+    modal = new bootstrap.Modal(modalEl, {});
+
+    modalEl.addEventListener('hidden.bs.modal', listener);
+    modal.show();
+
+    function listener() {
+      window.davinci.cleanup(activeWidget);
+      widgetWrapper.hideModalError();
+      activeWidget = null;
+      modal = null;
+      modalEl.removeEventListener('hidden.bs.modal', listener);
+      sessionStorage.removeItem('bxi_runningPolicyId');
+
+      if (widgetWrapper.HideLogo) {
+        modalEl.classList.remove('hide-vertical-logo');
+      }
+
+      if (isStaticWidget) {
+        launchStaticWidget();
+      }
+    }
+  }
+
+  function launchStaticWidget(widgetContainer, continueToken) {
+    if (!widgetContainer) {
+      widgetContainer = document.querySelector('[data-dv-flow="static"]');
+    }
+
+    const widgetWrapper = new FlowContainerWrapper(widgetContainer);
+
+    if (!widgetWrapper.PolicyId) {
+      throw Error('Policy ID is required to launch a flow.');
+    }
+
+    logger.log('Launching static flow');
+
+    launchFlow(widgetContainer, widgetWrapper, continueToken);
+  }
+
+  async function launchFlow(flowContainer, widgetWrapper, continueToken) {
+    if (activeWidget) {
+      window.davinci.cleanup(activeWidget);
+      activeWidget = null;
+    }
+
+    sessionStorage.setItem('bxi_runningPolicyId', widgetWrapper.PolicyId);
+
+    let tokenEndpoint = '/dvtoken';
+
+    const urlParams = {
+      apiKey: widgetWrapper.ApiKey,
+      companyId: widgetWrapper.CompanyId,
+    };
+
+    tokenEndpoint += Object.values(urlParams).find(key => key !== undefined) 
+      ? `?${new URLSearchParams(Object.keys(urlParams).reduce((acc, key) => urlParams[key] ? {...acc, [key]: urlParams[key]} : acc, {}))}`
+      : '';
+
+    logger.log('Calling token endpoint', tokenEndpoint);
+
+    const tokenResponse = await fetch(tokenEndpoint);
+    logger.log('Token response received', tokenResponse)
+    const tokenData = await tokenResponse.json();
+    logger.log('Token data parsed from response', tokenData);
+
+    if (tokenResponse.status !== 200) {
+      widgetWrapper.displayError(tokenData.error || 'An unkown error occured, see glitch server side logs for more details');
+      logger.error('Token response was not successful', tokenResponse);
+      return;
+    }
+    
+    const parameters = await widgetWrapper.getDvRequestParams();
+    logger.log('DaVinci request parameters', parameters);
+
+    const dvWidgetProps = {
+      config: {
+        method: continueToken ? 'continueFlow' : 'runFlow',
+        apiRoot: tokenData.apiRoot,
+        accessToken: continueToken || tokenData.token,
+        companyId: tokenData.companyId,
+        policyId: widgetWrapper.PolicyId,
+        parameters,
+      },
+      useModal: false,
+      successCallback: async response => {
+        logger.log('DaVinci flow successful', response);
+        if (widgetWrapper.SuccessCallback) {
+          logger.log('SuccessCallback exists on widget wrapper');
+          await bxi.callFunction(widgetWrapper.SuccessCallback, response);
+        }
+        
+        modal?.hide();   
+      },
+      errorCallback: async error => {
+        logger.error('DaVinci flow was not successful');
+        sessionStorage.removeItem('bxi_runningPolicyId');
+        if (widgetWrapper.ErrorCallback) {
+          logger.log('ErrorCallback exists on widget wrapper');
+          await bxi.callFunction(widgetWrapper.ErrorCallback, error);
+        }
+      }
+    };
+
+    logger.log('Rendering DV widget with request parameters', dvWidgetProps);
+    window.davinci.skRenderScreen(flowContainer, dvWidgetProps);
+
+    activeWidget = flowContainer;
+  }
+})();
+
---
+++ server.js
@@ -0,0 +1,207 @@
+/**
+ * This is the main Node.js server script for your project
+ * Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
+ */
+
+// Bring in .env file (happens automatically in glitch but not when running locally)
+import 'dotenv/config';
+
+// NodeJS imports
+import { fileURLToPath } from 'url';
+import path from 'path';
+import fs from 'fs';
+
+// External libraries
+import Fastify from 'fastify';
+import fetch from 'node-fetch';
+
+// Internal js files
+import helpers from './resources/helpers.js';
+import { initHandlebars } from './resources/handlebars.js';
+import Logger from './public/js/logger.js';
+
+// Initialize variables that are no longer available by default in Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Initialize internal variables
+const bxiEnvVars = helpers.getBxiEnvironmentVariables();
+const verticals = helpers.getVerticals();
+
+const debug = process.env.BXI_DEBUG_LOGGING === 'true';
+
+const logger = new Logger(debug);
+
+// Require the fastify framework and instantiate it
+const fastify = Fastify({
+  // Set this to true for detailed logging
+  logger: debug,
+  ignoreTrailingSlash: true
+});
+
+// Setup our static files (images and SCSS)
+fastify.register(import('@fastify/static'), {
+  root: path.join(__dirname, 'public'),
+  prefix: '/',
+});
+ 
+initHandlebars(fastify);
+
+// Our home page route
+// Redirects to the default vertical (if set in environment variables) or falls back on generic
+fastify.get('/', function (_, reply) {
+  const defaultVertical = process.env.BXI_ACTIVE_VERTICAL;
+  const redirectVertical = helpers.isValidVertical(defaultVertical) ? defaultVertical : 'generic';
+
+  logger.log(`Root hit, defined default vertical is: '${defaultVertical}' redirecting to: '${redirectVertical}'`);
+  reply.redirect(`/${redirectVertical}`);
+});
+
+fastify.get('/.well-known/security.txt', function (_, reply) {
+  logger.log(`/.well-known/securtiy.txt was hit, redirecting ping identity's version`);
+  reply.redirect(`http://www.pingidentity.com/.well-known/security.txt`);
+});
+
+// Get a dv token from the server, we do this in server.js as a security best practice so 
+// API Keys don't need to be exposed on the front-end
+fastify.get('/dvtoken', async function (request, reply) {
+  // Allow for apiKey and companyId overrides to come from front end, even though it's not encouraged
+  const apiKey = request?.query.apiKey || process.env.BXI_API_KEY;
+  const companyId = request?.query.companyId || process.env.BXI_COMPANY_ID;
+
+  const dvBaseUrl = `${process.env.BXI_API_URL}/`;
+  const dvSdkTokenBaseUrl = `${process.env.BXI_SDK_TOKEN_URL}/v1`;
+
+  const tokenRequest = {
+    method: 'GET',
+    headers: {
+      'X-SK-API-KEY': apiKey
+    }
+  };
+
+  const tokenResponse = await fetch(`${dvSdkTokenBaseUrl}/company/${companyId}/sdktoken`, tokenRequest); // Endpoint is case sensitive in Davinci V2
+  const parsedResponse = await tokenResponse.json();
+
+  if (!parsedResponse.success) {
+    logger.error('An error Occured');
+    logger.error('Parsed Response', parsedResponse);
+    logger.error('Raw', tokenResponse);
+    return reply.code(500).send({error: `An error occured getting DaVinci token. See Glitch server logs for more details, code: ${parsedResponse.httpResponseCode}, message: '${parsedResponse.message}'.`});
+  }
+
+  logger.log('Successfully retreived sdktoken for DaVinci', parsedResponse);
+
+  reply.send({
+    token: parsedResponse.access_token,
+    companyId: companyId,
+    apiRoot: dvBaseUrl
+  });
+});
+
+fastify.get('/docs', (request, reply) => {
+  const vertical = request.query.vertical || 'company';
+  const icons = fs.readdirSync('src/partials/icons').map(file => file.replace('.hbs', ''));
+  return reply.view('src/docs/index.hbs', {
+    selectedVertical: vertical,
+    verticals: verticals.filter(v => v !== 'generic'),
+    brandingPartial: () => `${vertical}Branding`,
+    icons: icons.map(icon => ({ icon: icon, partial: icon + 'Icon'}))
+  });
+});
+
+fastify.get('/verticals', (_, reply) => {
+  reply.code(200).header('Content-Type', 'application/json; charset=utf-8').send(verticals);
+});
+
+// Set up shortcuts endpoints, shows all verticals with applicable links
+fastify.get('/shortcuts', (_, reply) => {
+  const viewParams = verticals.map(vertical => {
+    const settings = helpers.getSettingsFile(vertical).settings;
+    return { 
+      name: settings.title, 
+      logo: settings.images.dialog_logo || settings.images.logo,
+      home: `/${vertical}`,
+      dashboard: vertical !== 'generic' ? `/${vertical}/dashboard` : '',
+      dialogExamples: vertical !== 'generic'? `/${vertical}/dialog-examples` : '',
+    };
+  });
+
+  logger.log('/shortcuts endpoint hit, sending view data', viewParams);
+  return reply.view('src/pages/shortcuts.hbs', viewParams);
+});
+
+// Generic does not have dashboard or dialog-examples page
+helpers.getVerticals().forEach(vertical => {
+  // Vertical Home Page
+  fastify.get(`/${vertical}`, function (_, reply) {
+    const viewParams = getViewParams(vertical);
+    logger.log(`/${vertical} hit, sending home page view data`, viewParams);
+    return reply.view(`src/pages/${vertical}/index.hbs`, viewParams);
+  });
+
+  // Generic does not have dashboard or dialog examples pages
+  if (vertical === 'generic') {
+    return;
+  }
+
+  // Vertical Dashboard Page
+  fastify.get(`/${vertical}/dashboard`, function (_, reply) {
+    const viewParams = getViewParams(vertical);
+    logger.log(`/${vertical}/dashboard hit, sending dashboard page view data`, viewParams);
+    return reply.view(`src/pages/${vertical}/dashboard.hbs`, viewParams);
+  });
+
+  // Vertical Dialog Examples Page
+  fastify.get(`/${vertical}/dialog-examples`, function (_, reply) {
+    const settings = helpers.getSettingsFile(vertical).settings;
+    const viewParams = { 
+      vertical, 
+      brandingPartial: () => `${vertical}Branding`,
+      dialogLogo: settings.images.dialog_logo,
+      favicon: settings.images.favicon || '/generic/favicon.ico',
+      appleTouchIcon: settings.images.apple_touch_icon || '/generic/apple-touch-icon.png',
+    };
+    logger.log(`/${vertical}/dialog-examples hit, sending view data`, viewParams);
+    return reply.view(`src/pages/dialog-examples.hbs`, viewParams);
+  });
+
+  // Redirect old /admin urls to dashboard
+  fastify.get(`/${vertical}/admin`, function (_, reply) {
+    logger.log(`/${vertical}/admin hit, redirecting to dashboard instead`)
+    reply.redirect(`/${vertical}/dashboard`);
+  });
+});
+
+// Just in case /generic/dashboard is hit, redirect to generic
+fastify.get('/generic/dashboard', (_, reply) => {
+  logger.log(`/generic/dashboard hit, redirecting to /generic since this doesn't exist`);
+  reply.redirect('/generic');
+});
+
+// Redirect 404s to base url rather than throwing errors
+fastify.setNotFoundHandler((_, reply) => {
+  logger.log('Invalid url, redirecting to root');
+  reply.redirect('/');
+});
+
+// Combine settings.json with environment parameters to be passed to handlebars templates/front-end
+// Please note .env parameters are manually whitelisted in resources/handlebars.js for security reasons
+function getViewParams(vertical) {
+  let params = helpers.getSettingsFile(vertical);
+  params.env = bxiEnvVars;
+  return params;
+}
+
+// Run the server and report out to the logs
+fastify.listen(
+  { port: process.env.PORT || 5000, host: '0.0.0.0' },
+  function (err, address) {
+    if (err) {
+      fastify.log.error(err);
+      process.exit(1);
+    }
+
+    console.log(`Your app is listening on ${address}`);
+    fastify.log.info(`server listening on ${address}`);
+  }
+);
